e4f18bdc8f899dc03ea6cbea88f9e4151201992a
Leonel-Javier-Calleja-Hernandez <leocallejas565@gmail.com.com>
2025-12-09 14:07:34 -0500

    arreglos

---
+++ introduccion/arreglos.js
@@ -0,0 +1,13 @@
+const arreglo = ['texto', 18.1212, false, {propiedad:"valor"},  [1,2,3] ];
+
+console.log(arreglo)
+
+const amigos = ['leonel', 'angel', 'daniel']
+
+amigos[3] = "salome"
+amigos[2] = "juan"
+console.log("el arreglo tiene "+ amigos.length + " amigos")
+
+
+amigos.push('dayana')
+console.log(amigos)
