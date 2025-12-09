543dd56fdcb6c67a152e3fdae6f557a150152096
Leonel-Javier-Calleja-Hernandez <leocallejas565@gmail.com.com>
2025-12-09 14:36:14 -0500

    templateStrings

---
+++ introduccion/templateStrings.js
@@ -0,0 +1,8 @@
+const nombre = 'leonel callejas';
+const edad = 20;
+const pais = 'colombia'
+
+console.log('La persona es ' + nombre + ' tiene ' + edad + ' y es de ' + pais )
+
+// MAS FACIL
+console.log(`nombre : ${nombre}, edad : ${edad}, pais : ${pais} `)
