5b9b06b089c9b2f2407fcfb47315f902f04a3358
Leonel-Javier-Calleja-Hernandez <leocallejas565@gmail.com.com>
2025-12-09 13:42:13 -0500

    Operadores aritméticos

---
+++ introduccion/index.js
@@ -0,0 +1,35 @@
+//variables y constantes
+let nombre = "leonel";
+let edad = 27;
+
+console.log(nombre,edad);
+
+//suma
+const SUMA = 4 + 4;
+console.log(SUMA)
+
+let texto =  "parrafo \"SUMA\""
+let num = 4
+var num2 = -40.23;
+
+let condicional = 2 < 10;
+console.log(condicional)
+
+
+array = [2,3,4,5,56]
+console.log(array[5])
+
+//objetos
+persona = {
+    nombre :'leonel',
+    edad : 18,
+    auto : "renalt",
+    color : "rojo",
+}
+console.log(persona.nombre, persona.edad)
+
+//funciones
+console.log(saludar())
+function saludar(){
+    return "holaaaaaaaa";
+}
---
+++ introduccion/operadores.js
@@ -0,0 +1,22 @@
+let resultado = 4 + 5;
+let resta = 10 - 32;
+
+let modulo = 14 % 3
+
+console.log(modulo)
+
+ num = 10
+ num++
+ console.log(num)
+
+ let cantidad = 20;
+cantidad += 5;
+console.log(cantidad)
+
+const comparar = 5 >= 10;
+console.log(comparar)
+
+
+//operador ternario o condicional
+const comprobacion = 7 > 1? 'es mayor' : 'es menor';
+console.log(comprobacion)
