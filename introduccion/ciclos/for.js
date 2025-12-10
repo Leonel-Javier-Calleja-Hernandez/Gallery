e854ae4f201e4eef950125fe187d47b402636655
Leonel-Javier-Calleja-Hernandez <leocallejas565@gmail.com.com>
2025-12-10 14:07:42 -0500

    for

---
+++ introduccion/ciclos/for.js
@@ -0,0 +1,40 @@
+const arrayNombres = [
+  "Leonel",
+  "Lucia",
+  "Lorena",
+  "Luis",
+  "Angel",
+  "Daniel",
+  "Carlos",
+  "Miguel",
+  "Sara",
+  "Valentina",
+  "Jorge",
+  "Andres",
+  "Kevin",
+  "Diana",
+  "Camila",
+  "Mateo",
+  "Sebastian",
+  "Julian",
+  "Natalia",
+  "Brayan"
+];
+
+//se puede ejecutar un bloque de codigo Mientras se cumpla una condicion
+for ( i = 0; i < arrayNombres.length; i++) {
+   console.log(arrayNombres[i])
+    
+}
+
+for (i = 1 ; i <= 10;i++ ){
+    console.log(i);
+    if (i == 10){
+        console.log("FIN IF 1 - 10")
+    }
+};
+
+//for del 50 al 1
+for(i = 50; i >= 0; i--){
+    console.log(i)
+}
